ccef50633f20431778e6d2d1abf39fefd0f5a67e
tniromin <tniromin2@gmail.com>
2023-10-12 14:32:55 +0530

    Added file to test frame

---
+++ src/test/java/com/HitnRun/FrameTest.java
@@ -0,0 +1,22 @@
+package com.HitnRun;
+
+
+import com.HitnRun.controllers.*;
+import com.HitnRun.views.*;
+
+
+public class FrameTest {
+	
+	public static void main(String args[]) {
+		
+			//Idiot you used a void function instead of a contructor dumb fuck -tniromin to tniromin
+			LoginController Lcontrol = new LoginController();
+
+			
+
+		}
+		
+		
+	
+
+}
